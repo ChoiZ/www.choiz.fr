b46f1db662dad5f9c9069880e1f6ee1edf64a662
François LASSERRE <choiz@me.com>
2019-04-26 00:43:00 +0200

    Update config for publishing

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = '//www.choiz.fr'
+SITEURL = 'https://www.choiz.fr'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
